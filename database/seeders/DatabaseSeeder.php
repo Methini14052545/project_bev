ac7c0802ee9e00a12a681f1ebde678823f7b0f8a
Methini14052545 <fmaypimploy@gmail.com>
2023-09-09 13:48:16 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -49,5 +49,106 @@ class DatabaseSeeder extends Seeder
             'inv_cost' => '720',
             'status' => 'AVAILABLE',
         ]);
+
+        Beverage::create([
+            'item_code' => '215-THBS',
+            'item_name' => 'RED WINE-Trinity Hill Hawkes Bay Syrah (07-WBG)',
+            'country' => 'NEW ZEALAND',
+            'uom' => 'BTL',
+            'commodity' => 'WINE',
+            'type' => 'RED WINE',
+            'inv_cost' => '720',
+            'status' => 'AVAILABLE',
+        ]);
+
+        Beverage::create([
+            'item_code' => '214-33W',
+            'item_name' => 'WHITE WINE-THAILAND, Colombard, MONSOON VALLEY COLOMBARD (10-WBG)',
+            'country' => 'THAILAND',
+            'uom' => 'BTL',
+            'commodity' => 'WINE',
+            'type' => 'WHITE WINE',
+            'inv_cost' => '452.80',
+            'status' => 'AVAILABLE',
+        ]);
+
+        Beverage::create([
+            'item_code' => '214-38W',
+            'item_name' => 'ROSE WINE-THAILAND, Blended Rose, Monsoon Valley Rose 75 Cl (12-WBG)',
+            'country' => 'THAILAND',
+            'uom' => 'BTL',
+            'commodity' => 'WINE',
+            'type' => 'ROSE WINE',
+            'inv_cost' => '410.70',
+            'status' => 'AVAILABLE',
+        ]);
+
+        Beverage::create([
+            'item_code' => '230200501',
+            'item_name' => 'WHITE WINE-Viognier Terre Siciliane (11-WBG)',
+            'country' => 'ITALY',
+            'uom' => 'BTL',
+            'commodity' => 'WINE',
+            'type' => 'WHITE WINE',
+            'inv_cost' => '756',
+            'status' => 'AVAILABLE',
+        ]);
+
+        Beverage::create([
+            'item_code' => '214-43R1',
+            'item_name' => 'RED WINE-THAILAND, Shiraz, MONSOON VALLEY SHIRAZ (05-WBG)',
+            'country' => 'THAILAND',
+            'uom' => 'BTL',
+            'commodity' => 'WINE',
+            'type' => 'RED WINE',
+            'inv_cost' => '501.26',
+            'status' => 'AVAILABLE',
+        ]);
+
+        Beverage::create([
+            'item_code' => '230-WCBR-001',
+            'item_name' => 'WINE CHAMPAGNE - Champagne Barons de Rothschild Brut (03-C)',
+            'country' => 'FRANCE',
+            'uom' => 'BTL',
+            'commodity' => 'WINE',
+            'type' => 'CHAMPAGNE WINE',
+            'inv_cost' => '2349.6',
+            'status' => 'AVAILABLE',
+        ]);
+
+        Beverage::create([
+            'item_code' => '230-JCEBN',
+            'item_name' => 'WINE CHAMPAGNE-FRANCE, Jacquesson, Cuvee 744, Extra Brut, NV (08-C)',
+            'country' => 'FRANCE',
+            'uom' => 'BTL',
+            'commodity' => 'WINE',
+            'type' => 'CHAMPAGNE WINE',
+            'inv_cost' => '2490',
+            'status' => 'AVAILABLE',
+        ]);
+
+        Beverage::create([
+            'item_code' => '244-539',
+            'item_name' => 'LIQUOR-Benedictine D.O.M. 70cl (09-L)',
+            'country' => '',
+            'uom' => 'BTL',
+            'commodity' => 'LIQUOR',
+            'type' => 'LIQUOR',
+            'inv_cost' => '1443.93',
+            'status' => 'AVAILABLE',
+        ]);
+
+        Beverage::create([
+            'item_code' => '244-554',
+            'item_name' => 'LIQUOR-Grand Marnier 70cl (29-L)',
+            'country' => '',
+            'uom' => 'BTL',
+            'commodity' => 'LIQUOR',
+            'type' => 'LIQUOR',
+            'inv_cost' => '1803.74',
+            'status' => 'AVAILABLE',
+        ]);
+
+        
     }
 }
